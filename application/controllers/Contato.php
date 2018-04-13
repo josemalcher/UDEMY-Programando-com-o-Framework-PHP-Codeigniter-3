68025f3224f42b239f247e5c98037985cc713c72
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-04-13 02:08:11 -0300

    66. Formulário de Contato - Parte 2
    + FIM DO CURSO!

--- application/controllers/Contato.php
+++ application/controllers/Contato.php
@@ -9,13 +9,14 @@ class Contato extends CI_Controller
         $this->categorias = $this->modelcategorias->listar_categorias();
     }
 
-    public function index()
+    public function index($enviado=null)
     {
         $this->load->helper('funcoes');
         $dados['categorias'] = $this->categorias;
         //Dados a serem enviados para o Cabeçalho
         $dados['titulo'] = 'Contato';
         $dados['subtitulo'] = '';
+        $dados['enviado'] = $enviado;
 
         $this->load->view('frontend/template/html-header', $dados);
         $this->load->view('frontend/template/header');
@@ -23,6 +24,40 @@ class Contato extends CI_Controller
         $this->load->view('frontend/template/aside');
         $this->load->view('frontend/template/footer');
         $this->load->view('frontend/template/html-footer');
+    }
+
+    public function enviar_mensagem(){
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('txtNome', 'Nome', 'required');
+        $this->form_validation->set_rules('txtEmail', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('txtMsg', 'Mensagem', 'required');
+
+        if ($this->form_validation->run()) {
+            $nome = $this->input->post('txtNome');
+            $email = $this->input->post('txtEMail');
+            $msg = $this->input->post('txtMsg');
+            $ip = $this->input->ip_address();
+
+            $this->load->library('email');
+            $this->email->from($email, $nome);
+            $this->email->to('teste@email.com'); // email que deseja receber as mensagem do form do contato
+            $this->email->subject('Formulário de contato - BLOG CI');
+            $this->email->message("
+                <p><strong>Nome: </strong> $nome</p>
+                <p><strong>Email: </strong> $email</p>
+                <p><strong>Mensagem: </strong> $msg</p>
+                <p><strong>IP: </strong> $ip </p>
+            ");
+            if($this->email->send()){
+                redirect(base_url("contato/1"));  //ENVIADO!
+            }else{
+                redirect(base_url("contato/2")); // ERRO
+            }
+
+        }else{
+            $this->index(); // campos vazios...
+        }
+
     }
 
 }
--- application/views/frontend/contato.php
+++ application/views/frontend/contato.php
@@ -10,22 +10,35 @@
 
             <div class="col-md-12">
                 <?php
+                    if($enviado == 1){
+                        echo '<div class="alert alert-success"> Email Enviado</div>';
+                    }else if ($enviado == 2) {
+                        echo '<div class="alert alert-warning"> EMAIL -NÃO- ENVIADO!!</div>';
+                    }
+                ?>
+                <?php
+                echo validation_errors('<div class="alert alert-danger">','</div>');
+
                 $atributosForm = array('name' => 'formulario_contato', 'id' => 'formulario_contato');
                 echo form_open(base_url('contato/enviar_mensagem'), $atributosForm);
 
-                $atribNome = array('name' => 'txtNome', 'id' => 'txtNome', 'class' => 'form-control', 'placeholder' => 'Digite seu Nome');
+                $atribNome = array( 'name' => 'txtNome',
+                                    'id' => 'txtNome',
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Digite seu Nome',
+                                    'value'=> set_value('txtNome'));
                 echo ("<div class='form-group'>") .
                     form_label("Nome", 'txtNome') .
                     form_input($atribNome) .
                     ("</div>");
 
-                $atribEmail = array('name' => 'txtEmail', 'id' => 'txtEmail', 'class' => 'form-control', 'placeholder' => 'Digite seu Email');
+                $atribEmail = array('name' => 'txtEmail', 'id' => 'txtEmail', 'class' => 'form-control', 'placeholder' => 'Digite seu Email','value'=> set_value('txtEmail'));
                 echo ("<div class='form-group'>") .
                     form_label("Email", 'txtEmail') .
                     form_input($atribEmail) .
                     (" </div>");
 
-                $atribMsg = array('name' => 'txtMsg', 'id' => 'txtMsg', 'class' => 'form-control', 'placeholder' => 'Digite sua mensagem');
+                $atribMsg = array('name' => 'txtMsg', 'id' => 'txtMsg', 'class' => 'form-control', 'placeholder' => 'Digite sua mensagem','value'=> set_value('txtMsg'));
                 echo ("<div class='form-group'>") .
                     form_label("Mensagem", 'txtMsg') .
                     form_textarea($atribMsg) .
